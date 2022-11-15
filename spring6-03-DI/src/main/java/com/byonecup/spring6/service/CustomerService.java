968296f0bf392f85dc941c3c1d01f7d921014857
turbogym <gymgym@88.com>
2022-11-15 21:58:19 +0800

    update 构造注入

---
+++ spring6-03-DI/src/main/java/com/byonecup/spring6/service/CustomerService.java
@@ -0,0 +1,25 @@
+package com.byonecup.spring6.service;
+
+import com.byonecup.spring6.dao.UserDao;
+import com.byonecup.spring6.dao.VIPDao;
+
+/**
+ * @Author Turbo
+ * @Description TODO
+ * @Date 2022/11/15 20:18
+ * @Version 1.0
+ */
+public class CustomerService {
+    private UserDao userDao;
+    private VIPDao vipDao;
+
+    public CustomerService(UserDao userDao, VIPDao vipDao) {
+        this.userDao = userDao;
+        this.vipDao = vipDao;
+    }
+
+    public void save() {
+        userDao.insert();
+        vipDao.insert();
+    }
+}
--- spring6-03-DI/src/test/java/com/byonecup/spring6/test/SpringDITest.java
+++ spring6-03-DI/src/test/java/com/byonecup/spring6/test/SpringDITest.java
@@ -1,6 +1,7 @@
 package com.byonecup.spring6.test;
 
 import com.byonecup.spring6.dao.UserDao;
+import com.byonecup.spring6.service.CustomerService;
 import com.byonecup.spring6.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -18,5 +19,18 @@ public class SpringDITest {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         UserService userServiceBean = applicationContext.getBean("userServiceBean", UserService.class);
         userServiceBean.saveUser();
+    }
+
+    @Test
+    public void testConstructDI() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
+        customerService.save();
+
+        CustomerService csService = applicationContext.getBean("csService", CustomerService.class);
+        csService.save();
+
+        CustomerService csService2 = applicationContext.getBean("csService2", CustomerService.class);
+        csService2.save();
     }
 }
