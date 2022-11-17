f271563769afeefcb7c38be8b62d6494404813bb
turbogym <gymgym@88.com>
2022-11-18 02:03:49 +0800

    Bean作用域之单例和多例

---
+++ spring6-04-scope/src/main/java/com/byonecup/spring6/bean/SpringBean.java
@@ -0,0 +1,14 @@
+package com.byonecup.spring6.bean;
+
+/**
+ * @Author Turbo
+ * @Description TODO
+ * @Date 2022/11/18 01:51
+ * @Version 1.0
+ */
+public class SpringBean {
+
+    public SpringBean() {
+        System.out.println("SpringBean的无参构造方法被执行...");
+    }
+}
---
+++ spring6-04-scope/src/test/java/com/byonecup/spring6/test/SpringBeanScopeTest.java
@@ -0,0 +1,36 @@
+package com.byonecup.spring6.test;
+
+import com.byonecup.spring6.bean.SpringBean;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author Turbo
+ * @Description TODO
+ * @Date 2022/11/18 01:53
+ * @Version 1.0
+ */
+public class SpringBeanScopeTest {
+    @Test
+    public void testBeanScope() {
+        /**
+         * 1. Spring默认情况下是如何管理这个Bean的：
+         *      默认情况下，Bean是单例的。(单例：singleton)
+         *      在Spring上下文初始化的时候实例化。
+         *      每一次调用getBean()方法的时候，都返回那个单例的对象。
+         * 2. 当把Bean的scope属性设置为prototype：
+         *      Bean是多例的。
+         *      Spring上下文初始化的时候，并不会初始化这些prototype的Bean。
+         *      每一次调用getBean()方法的时候，实例化该Bean对象。
+         *      prototype翻译为：原型。
+         */
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-scope.xml");
+
+        SpringBean sb = applicationContext.getBean("sb", SpringBean.class);
+        System.out.println(sb);
+
+        SpringBean sb2 = applicationContext.getBean("sb", SpringBean.class);
+        System.out.println(sb2);
+    }
+}
